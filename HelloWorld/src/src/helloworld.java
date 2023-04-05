d3526dd35061ebf7814edcaa09cb922bfe6f6442
zun <000wkdwnsgh@gmail.com>
2023-04-05 09:33:02 +0900

    added 'her world'

--- HelloWorld/src/src/helloworld.java
+++ HelloWorld/src/src/helloworld.java
@@ -3,6 +3,8 @@ package src;
 public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println("Hello his world");
+		System.out.println("Hello your world");
 	}
 	
 }
